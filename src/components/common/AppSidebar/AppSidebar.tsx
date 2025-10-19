b08722162f8102370ecf519d68f88e497e27fb19
z1mo0x <droweckiy@mail.ru>
2025-10-19 09:45:50 +0300

    structure

---
+++ src/components/common/AppSidebar/AppSidebar.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible"
+import { DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu"
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
+import { DropdownMenu, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
+import { Building, ChevronDown, ChevronUp, HomeIcon, User, Users } from "lucide-react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+type Props = {}
+
+export default function AppSidebar({ }: Props) {
+
+    const pathname = usePathname()
+
+    const items = [
+        {
+            title: 'Главная',
+            url: "/dashboard",
+            icon: HomeIcon
+        },
+        {
+            title: 'CRM',
+            url: "/clients",
+            icon: Users
+        },
+    ]
+
+    return (
+        <Sidebar collapsible="icon">
+            <SidebarHeader className="font-bold font text-xl flex gap-[10px] items-center flex-row">
+                <Building />
+                <span>Integra</span>
+            </SidebarHeader>
+            <SidebarContent>
+                <Collapsible>
+                    <SidebarGroup>
+                        <SidebarGroupLabel asChild>
+                            <CollapsibleTrigger className="flex justify-between items-center">
+                                <span>Управление заказами</span>
+                                <ChevronDown />
+                            </CollapsibleTrigger>
+                        </SidebarGroupLabel>
+                        <CollapsibleContent>
+                            <SidebarGroupContent>
+                                <SidebarMenu>
+                                    {items.map((item) => (
+                                        <SidebarMenuItem key={item.title} >
+                                            <SidebarMenuButton asChild isActive={item.url === pathname}>
+                                                <Link href={item.url}>
+                                                    <item.icon />
+                                                    <span>{item.title}</span>
+                                                </Link>
+                                            </SidebarMenuButton>
+                                        </SidebarMenuItem>
+                                    ))}
+                                </SidebarMenu>
+                            </SidebarGroupContent>
+                        </CollapsibleContent>
+                    </SidebarGroup>
+                </Collapsible>
+            </SidebarContent>
+            <SidebarFooter>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <SidebarMenuButton>
+                                    <User /> Пользователь
+                                    <ChevronUp />
+                                </SidebarMenuButton>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent side="top">
+                                <DropdownMenuItem>
+                                    <span>Выйти из профиля</span>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarFooter>
+        </Sidebar>
+    )
+}
